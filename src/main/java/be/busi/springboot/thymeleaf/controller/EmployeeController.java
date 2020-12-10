ef33d6a3c854c2ae64d52967f1c33d086e6354a2
astridtchouteng <astrid.tchouteng@gmail.com>
2020-12-10 15:16:45 +0100

    add new employee

--- src/main/java/be/busi/springboot/thymeleaf/controller/EmployeeController.java
+++ src/main/java/be/busi/springboot/thymeleaf/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import be.busi.springboot.thymeleaf.entity.Employee;
@@ -35,7 +37,30 @@ public class EmployeeController {
 		// add to the spring model
 		theModel.addAttribute("employees", employees);
 		
-		return "list-employees";
+		return "employees/list-employees";
+	}
+	
+	
+	@GetMapping("showFormForAdd")
+	public String showFormForAdd(Model model) {
+		
+		// create model attribute to bind form data
+		Employee employee = new Employee();
+		
+		model.addAttribute("employee", employee);
+		
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		
+		// save employee
+		
+		employeeService.save(employee);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
 	}
 }
 
